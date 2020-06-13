e0d26929ce500821379c47d206ffc03eec666986
EikaMikiku <e.i.k.a@outlook.com>
2020-06-13 12:24:53 +0100

    Init

--- CrispyDoomSplits/Program.cs
+++ CrispyDoomSplits/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace CrispyDoomSplits {
     class Program {
         static void Main(string[] args) {
+            var WIDTH = 300u;
+            var HEIGHT = 300u;
+            var wnd = new RenderWindow(new VideoMode(WIDTH, HEIGHT), "Hey");
+            wnd.Closed += new EventHandler((o, e) => ((RenderWindow)o).Close());
+            while(wnd.IsOpen) {
+                wnd.DispatchEvents();
+                wnd.Clear(Color.Black);
+                wnd.Display();
+            }
         }
     }
 }
