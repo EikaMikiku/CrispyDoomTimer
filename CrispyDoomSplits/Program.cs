9a7482ff946e7b4bfd15702e170a6f3ebcf09fc2
EikaMikiku <e.i.k.a@outlook.com>
2020-06-13 12:06:34 +0100

    Add project files.

---
+++ CrispyDoomSplits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrispyDoomSplits {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
